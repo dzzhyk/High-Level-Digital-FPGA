b1f0343f7cd438eee4e60da07b6282ef97a29211
dzzhyk <1354839386@qq.com>
2020-11-22 16:20:18 +0800

    2020年11月22日16:20:11

--- 实验1/lab1.py
+++ 实验1/lab1.py
@@ -6,10 +6,15 @@
 from pulp import *
 import numpy as np
 
-CHILD_NUMBER = 4  # 最大子节点数量
-NODE_LIST = []  # 节点列表
-ORIGIN_NODE_GRAPH = {}  # 原始图
-NEW_NODE_GRAPH = {}  # 结果图
+
+NODE_LIST = []              # 节点列表
+ORIGIN_NODE_GRAPH = {}      # 原始图
+CHILD_NUMBER = 4            # 最大子节点数量
+PE = 16                     # PE数量
+NODE_COUNT = 0              # 节点数量
+T = 0                       # 最大时间步
+II = 3                      # II数量
+
 
 # 子节点类
 class ChildNode:
@@ -19,7 +24,6 @@ class ChildNode:
 
     def __str__(self):
         return f"[ChildNode] node_id={self.node_id}, edge_type={self.edge_type}"
-
 
 # 节点类
 class Node:
@@ -31,7 +35,7 @@ class Node:
         self.t_end = t_end  # 结束
         self.node_type = node_type  # 节点类型
         self.has_parent = has_parent  # 是否有父节点
-        self.t_step = t_start - 1  # 时间步初始化为t_start-1
+        self.t_step = 0  # 时间步初始化为0
 
     def __str__(self):
         return f"[Node] now_id={self.now_id}, origin_id={self.origin_id}, child_list=[" + ",".join(
@@ -39,11 +43,13 @@ class Node:
                f"], t_start={self.t_start}, t_end={self.t_end}, node_type={self.node_type}, has_parent={self.has_parent}\n"
 
     def toFile(self):
-        return f"{self.now_id},{','.join([str(c.node_id) for c in self.child_list])},{self.node_type},{self.origin_id}\n"
+        return f"{self.now_id},{self.t_step},{','.join([str(c.node_id) for c in self.child_list])},{self.node_type},{self.origin_id}\n"
 
 
-# 读入函数
+# 读入数据
 def read(filename):
+    # 更新相关常数
+    global NODE_COUNT, T
     with open(filename, "r", encoding="utf-8") as fin:
         print("<=== 读入文件：" + filename)
         temp = fin.readline()
@@ -54,103 +60,37 @@ def read(filename):
             temp_start, temp_end, temp_type, temp_parent = [int(a) for a in
                                                             temp_ls[CHILD_NUMBER * 2 + 1:CHILD_NUMBER * 2 + 5]]
             NODE_LIST.append(Node(temp_id, temp_child, temp_start, temp_end, temp_type, temp_parent))
+
+            # 更新全局常数
+            NODE_COUNT += 1
+            T = max(T, temp_end+1)
             temp = fin.readline()
 
 
-# 输出函数
+# 输出结果
 def write(filename):
     with open(filename, "w", encoding="utf-8") as fout:
         print("===> 输出目标：" + filename)
         for node in NODE_LIST:
-            fout.write(str(node))
-        for node in NODE_LIST:
             fout.write(node.toFile())
 
 
-# 预处理输入数据得到原始图
+# 预处理输入数据
 def pre():
     for node in NODE_LIST:
         ORIGIN_NODE_GRAPH[str(node.origin_id)] = [str(c.node_id) for c in node.child_list if c.node_id != 0]
 
 
-# 输出精简图信息
+# 打印图
 def printG(G):
     for dic in G:
         print(dic, end=": ")
         print(G[dic])
 
-# 图拓扑排序
-# 返回结果节点列表 [2, 1, 3, 4]
-def topological(G):
-    in_degrees = dict((u, 0) for u in G)
-    for u in G:
-        for v in G[u]:
-            if v == '0': continue
-            in_degrees[v] += 1
-    Q = ['1']
-    res = []
-    while Q:
-        u = Q.pop()
-        res.append(u)
-        for v in G[u]:
-            in_degrees[v] -= 1
-            if in_degrees[v] == 0 and v != '1':
-                Q.append(v)
-    return res
 
-
-# 图逆拓扑排序
-# 返回结果节点列表 [4, 3, 1, 2]
-def reverse_topological(G):
-    # 首先反向建图
-    RG = {}
-    for u in G:
-        RG[u] = []
-    for u in G:
-        for v in G[u]:
-            if v == '0': continue
-            RG[v].append(u)
-    for u in RG:
-        RG[u] = sorted(list(set(RG[u])))  # 去重
-    return topological(RG)
-
-
-# ASAP调度
-def asap(G):
-
-    asap_output = []
-
-    afterV = topological(G)
-    print(afterV)
-
-    pointSet = set()
-
-    for u in G:
-        for t in G[u]:
-            pointSet.add(t)
-    # print(pointSet)
-
-    for v in afterV:
-        if v not in pointSet:
-            asap_output.append([v, 1])
-            print(asap_output)
-        else:
-            for a in asap_output:
-                if v in G[a[0]]:
-                    asap_output.append([a[0], a[1] + 1])
-
-    return asap_output
-
-
-
-# ALAP
-def alap():
-    # ALAP的关键是要倒着用ASAP调度，然后反转结果即可
-    # 将图中所有没有孩子节点先执行，并在图中删除这些已执行的节点，直到图中所有节点被删除，最后反转结果顺序
-    pass
-
-def ILP(pe, points, times, G, II):
-    prob = LpProblem("myProblem", LpMinimize)
+# 求解
+def ILP(pe, points, times, G, ii):
+    prob = LpProblem("lab1", LpMinimize)
 
     PE = LpVariable("PE", lowBound=0, upBound=pe, cat="Integer")
     prob += PE
@@ -158,75 +98,70 @@ def ILP(pe, points, times, G, II):
     vars = [LpVariable("x" + str(i) + "_" + str(k), lowBound=0, upBound=1, cat="Integer")
             for i in range(points) for k in range(times)]
 
-    print(vars)
+    print(f"=========================定义变量=========================\n{vars}")
 
     for i in range(points):
         prob += lpSum(vars[i * 8: i * 8 + times]) == 1
-
     vars = np.array(vars)
-    # print(vars)
 
-    print(G)
     for i in G:
-        print(G[i])
         values = G[i]
         for v in values:
-            # print(i)
-            # print(v)
             node_child = []
             node_father = []
-
             for t in range(times):
-                node_child.append(vars[(int(v)-1)*8+t] * t)
-                # print("v+t")
-                # print((int(v)-1)*8+t)
-                node_father.append(vars[(int(i)-1)*8+t] * t)
+                node_child.append(vars[(int(v) - 1) * 8 + t] * t)
+                node_father.append(vars[(int(i) - 1) * 8 + t] * t)
             prob += lpSum(node_child) >= lpSum(node_father) + 1
 
     for j in range(times):
         pe_cons = []
         for i in range(points):
-            pe_cons.append(vars[i*8+j])
+            pe_cons.append(vars[i * 8 + j])
         prob += lpSum(pe_cons) <= PE
 
-# ii
-    maxListNum = np.math.ceil(times / II)  # 循环次数
-    for i in range(II):
+    # 循环次数
+    maxListNum = np.math.ceil(times / ii)
+    for i in range(ii):
         temp_ii_cons = []
         for k in range(maxListNum):
-            if(i+k*II) < times:
+            if (i + k * ii) < times:
                 for p in range(points):
-                    temp_ii_cons.append(vars[p*times + i+k*II])
+                    temp_ii_cons.append(vars[p * times + i + k * ii])
         prob += lpSum(temp_ii_cons) <= PE
 
-    print(prob)
+    # 打印所有约束
+    print(f"=========================约束信息=========================\n{prob}")
 
-    status = prob.solve()
-    for i in range(len(vars)):
-        if vars[i].varValue == 1:
-            print(i)
+    # 求解
+    print("=========================进行求解=========================")
+    prob.solve()
+
+    print("=========================输出结果=========================")
     for i in range(points):
         timeList = []
         for j in range(times):
-            timeList.append(j*vars[i*8 + j].varValue)
-        print(i+1,end="")
-        print("节点时间步是", end="")
-        print(sum(timeList))
-
+            timeList.append(j * vars[i * 8 + j].varValue)
+        temp_step = int(sum(timeList))
+        # 把结果添加到NODE_LIST中的节点信息中
+        NODE_LIST[i].t_step = temp_step
+        print(f"{i + 1} 节点时间步是 {temp_step}")
 
 
 if __name__ == "__main__":
+
+    # 读入数据
     read("./in")
-    write("./out")
-    pre()  # 预处理得到精简图
+
+    # 预处理得到精简图
+    pre()
+
+    # 打印精简图信息
+    print("=========================获取图信息=========================")
     printG(ORIGIN_NODE_GRAPH)
-    ans = topological(ORIGIN_NODE_GRAPH)
-    # ans_reversed = reverse_topological(ORIGIN_NODE_GRAPH)
-    # print(ans)
-    # print(ans_reversed)
-    #
-    # asap_res = asap(G=ORIGIN_NODE_GRAPH)
-    # print(asap_res)
 
+    # 求解
+    ILP(PE, NODE_COUNT, T, ORIGIN_NODE_GRAPH, II)
 
-    ILP(16, 11, 8, ORIGIN_NODE_GRAPH, 3)
+    # 输出
+    write("./out")
