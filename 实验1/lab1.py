9a2a86993efc1c71ce4984bffb17fc42fe90253a
dzzhyk <1354839386@qq.com>
2020-11-22 14:49:39 +0800

    2020年11月22日14:49:38

--- 实验1/lab1.py
+++ 实验1/lab1.py
@@ -3,11 +3,14 @@
 # date：2020年10月25日16:33:48
 # title：lab1.py
 
-import pulp
+from pulp import *
 
 CHILD_NUMBER = 4  # 最大子节点数量
 NODE_LIST = []  # 节点列表
-ORIGIN_NODE_GRAPH = {}  # 原始图
+ORIGIN_NODE_GRAPH = {}  # 简化过后的原始图，只有节点关系数据，用于拓扑排序
+AFTER_TOPO = []
+AFTER_RETOPO = []
+AFTER_ASAP_ALAP = []  # 使用ASAP_ALAP调度过后的图节点列表
 NEW_NODE_GRAPH = {}  # 结果图
 
 
@@ -31,7 +34,7 @@ class Node:
         self.t_end = t_end  # 结束
         self.node_type = node_type  # 节点类型
         self.has_parent = has_parent  # 是否有父节点
-        self.t_step = t_start - 1  # 时间步初始化为t_start-1
+        self.t_step = t_start - 1  # 节点step
 
     def __str__(self):
         return f"[Node] now_id={self.now_id}, origin_id={self.origin_id}, child_list=[" + ",".join(
@@ -39,7 +42,7 @@ class Node:
                f"], t_start={self.t_start}, t_end={self.t_end}, node_type={self.node_type}, has_parent={self.has_parent}\n"
 
     def toFile(self):
-        return f"{self.now_id},{','.join([str(c.node_id) for c in self.child_list])},{self.node_type},{self.origin_id}\n"
+        return f"{self.now_id},{self.t_step},{','.join([str(c.node_id) for c in self.child_list])},{self.node_type},{self.origin_id}\n"
 
 
 # 读入函数
@@ -53,7 +56,9 @@ def read(filename):
             temp_child = [ChildNode(int(temp_ls[i * 2 + 1]), int(temp_ls[i * 2 + 2])) for i in range(CHILD_NUMBER)]
             temp_start, temp_end, temp_type, temp_parent = [int(a) for a in
                                                             temp_ls[CHILD_NUMBER * 2 + 1:CHILD_NUMBER * 2 + 5]]
+
             NODE_LIST.append(Node(temp_id, temp_child, temp_start, temp_end, temp_type, temp_parent))
+            AFTER_ASAP_ALAP.append(Node(temp_id, temp_child, temp_start, temp_end, temp_type, temp_parent))
             temp = fin.readline()
 
 
@@ -70,7 +75,7 @@ def write(filename):
 # 预处理输入数据得到原始图
 def pre():
     for node in NODE_LIST:
-        ORIGIN_NODE_GRAPH[str(node.origin_id)] = [str(c.node_id) for c in node.child_list if c.node_id!=0]
+        ORIGIN_NODE_GRAPH[str(node.origin_id)] = [str(c.node_id) for c in node.child_list if c.node_id != 0]
 
 
 # 输出精简图信息
@@ -83,19 +88,25 @@ def printG(G):
 # 图拓扑排序
 # 返回结果节点列表 [2, 1, 3, 4]
 def topological(G):
-    in_degrees = dict((u, 0) for u in G)
+    in_degree = dict((u, 0) for u in G)
     for u in G:
         for v in G[u]:
             if v == '0': continue
-            in_degrees[v] += 1
-    Q = ['1']
+            in_degree[v] += 1
+
+    Q = []
+
+    for key, value in in_degree.items():
+        if key != '0' and value == 0:
+            Q.append(key)
+
     res = []
-    while Q:
+    while len(Q) > 0:
         u = Q.pop()
         res.append(u)
         for v in G[u]:
-            in_degrees[v] -= 1
-            if in_degrees[v] == 0 and v!='1':
+            in_degree[v] -= 1
+            if in_degree[v] == 0:
                 Q.append(v)
     return res
 
@@ -118,12 +129,45 @@ def reverse_topological(G):
 
 # ASAP调度
 def asap():
-    pass
+    # 求解PIs
+    PIs = set()
+    for node in ORIGIN_NODE_GRAPH.values():
+        for i in node:
+            PIs.add(i)
+
+    for v in AFTER_TOPO:
+        act_v = None
+        # 首先找到节点原有对象
+        for node in AFTER_ASAP_ALAP:
+            if node.origin_id == eval(v):
+                act_v = node
+                break
+        if v in PIs:
+            act_v.new_id = 1
+        else:
+            # 找到最前面的节点
+            pass
 
 
-# ALAP
+# ALAP调度
 def alap():
     pass
+
+
+# 求解整数规划
+def solve_ilp(objective, constraints):
+    prob = pulp.LpProblem('LP1', pulp.LpMinimize)
+    prob += objective
+    for cons in constraints:
+        prob += cons
+    print(prob)
+    # 判断是否求解成功
+    status = prob.solve()
+    if status != 1:
+        return None
+    else:
+        # 返回整数解
+        return [v.varValue.real for v in prob.variables()]
 
 
 if __name__ == "__main__":
@@ -131,7 +175,29 @@ if __name__ == "__main__":
     # write("./out")
     pre()  # 预处理得到精简图
     printG(ORIGIN_NODE_GRAPH)
-    ans = topological(ORIGIN_NODE_GRAPH)
-    ans_reversed = reverse_topological(ORIGIN_NODE_GRAPH)
-    print(ans)
-    print(ans_reversed)
+    AFTER_TOPO = topological(ORIGIN_NODE_GRAPH)
+    AFTER_RETOPO = reverse_topological(ORIGIN_NODE_GRAPH)
+
+    # 变量，直接设置下限，并设置cat类型为整数解
+    X1 =
+    X2 = pulp.LpVariable('X2', lowBound=3, cat=pulp.LpInteger)
+    X3 = pulp.LpVariable('X3', lowBound=0, cat=pulp.LpInteger)
+
+    N = 10
+    temp = [k for k in range(1, N+1)]
+    vars = ["x"+str(i) for i in temp]
+    touse = []
+
+    for v in vars:
+        for i in range(1, N+1):
+            touse.append(v+str(i))
+
+    # 变量
+    variables = []
+    variables = [LpVariable(x, lowBound=0, upBound=1) for x in touse]
+
+    cons = []
+    cons.append(sum([a1[i] * variables[i] for i in range(0, V_NUM)]) <= 1000000)
+
+    res = solve_ilp()
+    print(res)
---
+++ 实验1/test.py
@@ -0,0 +1,4 @@
+ls = ['aaa', 'bbb', 'ccc', 'aaa', 'aaa']
+ans = sorted(list(set(ls)))
+print(ans)
+
